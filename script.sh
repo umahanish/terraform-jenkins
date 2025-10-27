b38ad62d305ae0658419f0b3b64645ee414b1d8c
umapathy <umahanish@gmail.com>
2025-10-27 20:52:06 +0300

    Update script to install Java and download Tomcat
    Install Java 11 and Amazon Corretto 17, configure JAVA_HOME, and download Apache Tomcat.

--- script.sh
+++ script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 sudo yum update –y
 #sudo amazon-linux-extras install java-openjdk11 -y
+sudo dnf install java-openjdk11 -y
+sudo dnf install -y java-17-amazon-corretto
+java -version
+readlink -f $(which java)
+echo 'export JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto' >> ~/.bashrc
+echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
 cd /opt/
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.48/bin/apache-tomcat-10.1.48.tar.gz 
 ls
